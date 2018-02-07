2cbcbc75251f309221e772d89fde02ec3dd9704e
Balazs Molnar <bmolnar@slb.com>
2018-02-07 22:09:32 +0100

    change lighting a bit

--- Autonomous/GameObjects/LigthingEffect.cs
+++ Autonomous/GameObjects/LigthingEffect.cs
@@ -18,16 +18,22 @@ namespace MonoGameTry.GameObjects
         {
             effect.LightingEnabled = true;
 
-            effect.DirectionalLight0.DiffuseColor = new Vector3(0.3f, 0.3f, 0.3f);
-            effect.DirectionalLight0.Direction = new Vector3(1, -7, -2);
-            effect.DirectionalLight0.SpecularColor = new Vector3(0.03f, 0.01f, 0.02f);
+            effect.DirectionalLight0.DiffuseColor = new Vector3(.6f, 0.6f, 0.6f);
+            var v = new Vector3(2f, -2f, -2f);
+            v.Normalize();
+            effect.DirectionalLight0.Direction = v;
+            effect.DirectionalLight0.SpecularColor = new Vector3(0.4f, 0.4f, 0.4f);
 
-            effect.DirectionalLight1.DiffuseColor = new Vector3(0.3f, 0.2f, 0.2f);
-            effect.DirectionalLight1.Direction = new Vector3(-2, 1, 100f);
-            effect.DirectionalLight1.SpecularColor = new Vector3(0.1f, 0.03f, 0.2f);
+            //effect.DirectionalLight1.DiffuseColor = new Vector3(0.7f, 0.7f, 0.7f);
+            //v = new Vector3(2f, -2f, 100f);
+            //v.Normalize();
+            //effect.DirectionalLight1.Direction = v;
+            //effect.DirectionalLight1.SpecularColor = new Vector3(0.1f, 0.03f, 0.2f);
+            
+            effect.AmbientLightColor = new Vector3(0.5f, 0.5f, 0.5f);
 
-            effect.FogStart = 0;
-            effect.FogEnd = 100;
+//            effect.FogStart = 0;
+//            effect.FogEnd = 100;
         }
     }
 }
