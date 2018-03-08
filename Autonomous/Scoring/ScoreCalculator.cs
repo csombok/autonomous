8a87b705313dbd7d049cc4877fc6f9cb2080357c
csombok@gmail.com <csombok@gmail.com>
2018-03-08 16:45:17 +0100

    added logic to handle tie game

--- Autonomous/Scoring/ScoreCalculator.cs
+++ Autonomous/Scoring/ScoreCalculator.cs
@@ -9,6 +9,7 @@ namespace Autonomous.Impl.Scoring
     internal class ScoreCalculator
     {
         private readonly ScoreCsvImporter _scoreImporter;
+        private const float TieGameTrashold = 0.0001f;
 
         public ScoreCalculator(ScoreCsvImporter scoreImporter)
         {
@@ -19,13 +20,27 @@ namespace Autonomous.Impl.Scoring
         {
             var sortedPlayers = players.OrderByDescending(car => car.Y);
 
+
             int position = 1;
+            float previousY = -1;
+            int previousScore = 0;
             foreach (var player in sortedPlayers)
             {
                 var distance = (int)Math.Round(player.Y);
                 var damageInPercent = (int)Math.Round(player.Damage * 100);
                 var speed = (int)Math.Round(player.VY * 4);
-                var score = player.Stopped || position > 3 ? 0 : (int)Math.Pow(2, 4 - position);
+                
+                var score = player.Stopped || position > 3 
+                    ? 0 
+                    : (int)Math.Pow(2, 4 - position);
+
+                if (!player.Stopped && Math.Abs(player.Y - previousY) < TieGameTrashold)
+                {
+                    score = previousScore;
+                }
+
+                previousY = player.Y;
+                previousScore = score;
 
                 yield return new PlayerScore(player.PlayerName,
                     distance, position, damageInPercent, speed, timeElapsed, player.Stopped, score);
